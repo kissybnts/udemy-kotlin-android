2c70b5212b8162ac1e2c61615b241804f1880811
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2018-01-18 19:42:13 +0600

    Lecture 8, Dinner Dicider Part 3

--- app/src/main/java/com/kissybnts/udemy_kotlin_android/MainActivity.kt
+++ app/src/main/java/com/kissybnts/udemy_kotlin_android/MainActivity.kt
@@ -3,13 +3,25 @@ package com.kissybnts.udemy_kotlin_android
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Japanses", "Chinese", "Italian", "Korean", "Pizza", "Hamburger")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        selectedDinnerText.text = "Japanese"
+        decideButton.setOnClickListener {
+            val i = Random().nextInt(foodList.count())
+            selectedDinnerText.text = foodList[i]
+        }
+
+        addFoodButton.setOnClickListener {
+            val newFood = newFoodText.text.toString()
+            foodList.add(newFood)
+            newFoodText.text.clear()
+        }
     }
 }
